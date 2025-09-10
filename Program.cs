1944ce64197e0ab28efad4e8267eebd957a42b88
JSnow985 <jaden.olvera@students.snow.edu>
2025-09-10 12:59:43 -0600

    basic string collection

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@ Console.Title = "Trip Planner!";
 
 Console.Write("Welcome to Jaden's Trip Planner! Press any key to get started!"); //introduction to trip planner, use Write with a space in the string to match example
 Console.ReadKey(true);
-Console.Clear();
 
+Console.Clear();
 Console.WriteLine("Driver"); //name of person driving
 string driverName = Console.ReadLine()!;
 Console.Clear();
@@ -45,28 +45,26 @@ while (dubDistanceMi <= 0) // Loop to make sure we get a useable number for late
     }
 }
 
-//Console.Clear();
-Console.Write(dubDistanceMi);
+Console.Clear();
+Console.WriteLine("What will your average speed be? (mph)"); //ask for average speed
+string stringAvgSpeed = Console.ReadLine()!;
 
-/*
-if (double.TryParse(stringDistanceMi, out double dubDistanceMi)) ;
-else
-{
-    Console.WriteLine("I'm sorry but we couldn't figure out what you meant!");
-    Environment.Exit(1);
-}
-*/
-/*
-Console.WriteLine("placeholder"); //ask for average speed
+Console.Clear();
+Console.WriteLine("What kind of gas mileage does your ride get? (mi/g)");
+string stringMPG = Console.ReadLine()!;
 
-Console.WriteLine("placeholder"); //ask for gas mileage
+Console.Clear();
+Console.WriteLine("How many passengers will you be taking? Not including the driver, of course!");
+string stringPassengers = Console.ReadLine()!;
 
-Console.WriteLine("placeholder"); //how many riders not including driver
+Console.Clear();
+Console.WriteLine("What currency do you prefer to use? ($, £, ¥, €)");
+string stringCurrency = Console.ReadLine()!;
 
-Console.WriteLine("placeholder"); //what currency we're going to use
+Console.Clear();
+Console.WriteLine("What's the current price of a gallon of fuel? ($/gal)"); //fuel price per gallon in dollars
+string stringFuelPrice = Console.ReadLine()!;
 
-Console.WriteLine("placeholder"); //fuel price per gallon in dollars
-*/
 /*
 Average song length and songs needed for the playlist
 could be cool to have a "this is how many times you could listen to this"
